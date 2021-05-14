85808109ecd9cee0f293be2e895faed862fcaf75
zangsu <76612738+zangsu@users.noreply.github.com>
2021-05-15 01:04:54 +0900

    Create print.py
    asdf

---
+++ print.py
@@ -0,0 +1,16 @@
+i = 0
+
+if((i and i-1) == 0):
+    print(1)
+    
+# string = input()
+# i = len(string)
+# # result = string * i
+# print(result)
+a = "Hello goorm!"
+b = a[-1] + a[-2] + a[-3]
+c = a[-0]
+print(b)
+print(c)
+d = b[0:2]
+print(d)
