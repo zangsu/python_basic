10878dc4a1bc76c1efab5ff84857dfc5d926184a
zangsu <76612738+zangsu@users.noreply.github.com>
2021-05-17 22:43:50 +0900

    Add files via upload
    원래 16일 껀데 늦게 올렸음,,,

---
+++ 210516.py
@@ -0,0 +1,10 @@
+first = "first"
+second = "second"
+# print("first = {0:^10}, second = {1:>10}".format(first, second))
+# print(f"first={first}, second= {second*2}")
+print(first.count(' '))
+print(first.find('i'))
+print(" ".join(first))
+str = "hi, my name is JangHyeoksu"
+num = len(str)
+print(num)
