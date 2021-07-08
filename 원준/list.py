48ae0ceceaadba786907920ef85cdc585bd8216d
zangsu <76612738+zangsu@users.noreply.github.com>
2021-07-08 21:15:43 +0900

    Add files via upload

--- 원준/list.py
+++ 원준/list.py
@@ -1,4 +1,5 @@
 from random import *
+from math import *
 def list1():
     list=[('다현', 200), ('정연',150), ('쯔위', 90), ('사나', 30), ('지효',15)]
     print(list)
@@ -55,8 +56,7 @@ def addmail(list, name, Email):#이메일 사전순으로 정렬a
 class node:
     def __init__(self, num):
         self.num = num
-        self.next = None
-    
+        self.next = None    
 def list4():#뽑은 순서대로 라고 해서 그대로 뒀음
     for i in range(6):
         if i == 0:
@@ -75,4 +75,88 @@ def list4():#뽑은 순서대로 라고 해서 그대로 뒀음
     while a != None:
         print(a.num, end=" ")
         a = a.next
-list4()
+def list5():#일단 생성까진 했으ㅁ
+    list = []
+    for i in range(10):
+        list.append(store(chr(ord('A')+i)))
+        print(list[i].data[0], list[i].len)
+    min = 0
+    for i in range(1, 10):
+        if list[i].len < list[min].len:
+            min = i
+    head = headdep(list[min])
+    now = head
+    del list[min]
+    while list != None:
+        min = 0
+        for i in range(len(list)):
+            if list[i].len < list[min].len:
+                min = i
+        now.next = dep(head, list[min])
+        now = now.next
+        del list[min]
+    now = now.next
+    while 1:
+        print(f"{now.data.data[0]} 편의점, 거리 : {now.data.len}")
+        now = now.next
+        if now == head:
+            break
+        
+
+
+class dep:
+    def __init__(self, a, data):
+        self.next = a
+        self.data = data
+class headdep(dep):
+    def __init__(self, data):
+        self.next = self
+        self.data = data
+def pow(a, b):
+    if b == 1:
+        return a
+    else:
+        return a * pow(a, b-1)
+def findlen(store):
+     return sqrt(pow(store.data[1],2)+pow(store.data[2],2))
+class store:
+    def __init__(self, name):
+        x = randint(1, 101)
+        y= randint(1, 101)
+        self.data = (name, x, y)
+        self.len = findlen(self)
+def list6():
+    temp = 0
+    a = headnode('다현')
+    b = bside('정연', a)
+    c = bside('쯔위', b)
+    d = bside('사나', c)
+    e = bside('지효', d)
+    a.prev = e
+    e.next = a
+    head = a
+    print("정방향 --> ", end="")
+    while 1:
+        print(a.name, end=" ")
+        a = a.next
+        if a == head:
+            break
+    print()
+    print("역방향 --> ", end="")
+    while 1:
+        print(a.prev.name, end=" ")
+        a = a.prev
+        if a == head:
+            break
+class bside:
+    def __init__(self, name, prev):
+        self.prev = prev
+        self.name = name
+        self.next = None
+        self.prev.next = self
+class headnode(bside):
+    def __init__(self, name):
+        self.name = name
+        self.next = self
+        self.prev = self
+list5()
