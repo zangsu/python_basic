3f2e29f9b5d9fd3c274e22300a77bd6fd8cd788f
zangsu <76612738+zangsu@users.noreply.github.com>
2021-07-15 15:26:47 +0900

    Add files via upload

--- 원준/stack.py
+++ 원준/stack.py
@@ -1,4 +1,3 @@
-top = -1
 def stack1():
     path = ['주황', '초록', '파랑', '보라', '빨강', '노랑']
     top = len(path)-1
@@ -6,18 +5,38 @@ def stack1():
     for i in range(top+1):
         print(f"{path[i]} --> ", end ="")
     print('과자집')
-    for i in range(top):
-        print(f"{path[top-1]} -->", end="")
-        path = pop(path, top)
-        #vsc작동 문제! 오류 찾아내기
+    print('우리집에 가는길 : ', end ="")
+    for i in range(top+1):
+        print(f"{path[top]} --> ", end="")
+        top -=1
+    print('우리집')
 def push(list, top, data):
     list.append(data)
     top += 1
     return list
-def pop(list, top):
-    print(list[top])
-    top -= 1
-    del list[top]
-    return list
+def stack2():
+    top = -1
+    script = []
+    script.append('진달래꽃')
+    top += 1
+    script.append('나 보기가 역겨워')
+    top += 1
+    script.append('가실 때에는')
+    top += 1
+    script.append('말없이 고이 보내드리오리다.')
+    top += 1
 
-stack1()
+    print('----- 원본 -----')
+    for i in range(top+1):
+        print(script[i])
+    print('----- 거꾸로 처리된 결과-----')
+    for i in range(top+1):
+        linetop = len(script[top])-1
+        for j in range(len(script[top])):
+            print(script[top][linetop], end = "")
+            linetop -= 1
+        top -= 1
+        print()
+def queue1():
+    
+queue1()
